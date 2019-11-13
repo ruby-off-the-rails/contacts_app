75760f0b2ecf6e6f4e7041d7cf7a50e7446fe484
brian <riceb53@gmail.com>
2019-11-12 19:28:46 -0600

    add middle name and bio to contact, make requisite changes in views and controller

--- app/controllers/api/contacts_controller.rb
+++ app/controllers/api/contacts_controller.rb
@@ -12,10 +12,13 @@ class Api::ContactsController < ApplicationController
   def create
     @contact = Contact.new(
       first_name: params[:first_name],
+      middle_name: params[:middle_name],
       last_name: params[:last_name],
       email: params[:email],
-      phone_number: params[:phone_number]
+      phone_number: params[:phone_number],
+      bio: params[:bio]
     )
+    @contact.save
     render 'show.json.jb'
   end
 
@@ -23,8 +26,10 @@ class Api::ContactsController < ApplicationController
     @contact = Contact.find_by(id: params[:id])
     @contact.update(
       first_name: params[:first_name],
+      middle_name: params[:middle_name],
       last_name: params[:last_name],
       email: params[:email],
+      bio: params[:bio],
       phone_number: params[:phone_number]
     )
     render 'show.json.jb'
