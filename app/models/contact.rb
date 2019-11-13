3ae6e82a5db015205466feffebf578bf8be234b9
brian <riceb53@gmail.com>
2019-11-12 18:58:01 -0600

    add model methods to view

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,2 +1,13 @@
 class Contact < ApplicationRecord
+  def friendly_created_at
+    created_at.strftime("%m/%d/%Y")
+  end
+
+  def full_name
+    "#{first_name} #{last_name}"
+  end
+
+  def japanese_phone_number
+    "+81 #{phone_number}"
+  end
 end
