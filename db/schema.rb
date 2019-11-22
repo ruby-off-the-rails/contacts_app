91114441dc691d57d5f3ae158391714c444d0c74
brian <riceb53@gmail.com>
2019-11-21 21:11:24 -0600

    add groups and contact groups

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_11_20_003113) do
+ActiveRecord::Schema.define(version: 2019_11_22_030532) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "contact_groups", force: :cascade do |t|
+    t.integer "contact_id"
+    t.integer "group_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "contacts", force: :cascade do |t|
     t.string "first_name"
@@ -27,6 +34,12 @@ ActiveRecord::Schema.define(version: 2019_11_20_003113) do
     t.float "lat"
     t.float "lng"
     t.integer "user_id"
+  end
+
+  create_table "groups", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
   create_table "users", force: :cascade do |t|
